0c1948ec91e5fdba3842120183b651243a6205fd
Abdul Basit <abrk030@gmail.com>
2021-02-23 21:29:42 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+// Home Nav
+try {
+    const headerNavToggle = document.querySelector('.header-nav-toggle');
+    const headerNav = document.querySelector('.main_nav');
+    const headerLogo = document.querySelector('.logo img');
+    let navOpen = false;
+    headerNavToggle.addEventListener('click', headerNavFunc);
+    function headerNavFunc() {
+        headerNav.classList.toggle('active');
+        if (!navOpen) {
+            navOpen = true;
+            setTimeout(() => {
+                headerLogo.src = './img/logo-white.png';
+            }, 500);
+        } else {
+            navOpen = false;
+            setTimeout(() => {
+                headerLogo.src = './img/logo.png';
+            }, 250);
+        }
+    }
+
+} catch (err) {
+    console.log(err);
+}
+
+// header scroll Func
+try {
+    document.addEventListener("scroll", () => {
+        if (scrollY > 100) {
+            document.querySelector("header").classList.add("header_shadow");
+
+        }
+        else {
+            document.querySelector("header").classList.remove("header_shadow");
+        }
+    })
+
+} catch (err) {
+    console.log(err);
+}
+// deactivate menu effect Func
+try {
+    var headerListLinks = document.querySelectorAll('.header_list_2_link');
+    headerListLinks.forEach(item => { item.addEventListener('click', headerNavFunc) });
+} catch (err) {
+    console.log(err);
+}
+
+// form Func
+try {
+    const inputs = document.querySelectorAll('.input');
+    function focusFunc() {
+        let parent = this.parentNode.parentNode;
+        parent.classList.add('focus');
+    }
+    function blurFunc() {
+        let parent = this.parentNode.parentNode;
+        if (this.value == "") {
+            parent.classList.remove('focus');
+        }
+    }
+    inputs.forEach(input => {
+        input.addEventListener('focus', focusFunc);
+        input.addEventListener('blur', blurFunc);
+    });
+} catch (err) {
+    console.log(err);
+}
+
+// prevent default
+document.querySelector('body').addEventListener('click', preventDflt);
+function preventDflt(e) {
+    var tElment = e.target.getAttribute('href');
+    var tElmentP = e.target.parentElement.getAttribute('href');
+    if (tElment == '#' || tElmentP == '#') {
+        e.preventDefault();
+    }
+}
+
+try {
+    const selected = document.querySelector(".selected");
+    const optionsContainer = document.querySelector(".options-container");
+
+    const optionsList = document.querySelectorAll(".option");
+
+    selected.addEventListener("click", () => {
+        optionsContainer.classList.toggle("active");
+    });
+
+    optionsList.forEach(o => {
+        o.addEventListener("click", () => {
+            selected.innerHTML = o.querySelector("label").innerHTML;
+            optionsContainer.classList.remove("active");
+        });
+    });
+} catch (err) {
+    console.log(err);
+}
